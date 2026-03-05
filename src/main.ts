866d32a350b3edde69a7702de054fb58fa640e08
shakeel7521951 <shakeel7521951@gmail.com>
2026-03-05 13:16:53 +0500

    feat: add allowed origin for CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
+    'https://oud-xi.vercel.app',
   ];
 
   const frontendUrl = configService.get<string>('FRONTEND_URL');
