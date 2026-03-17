930bc6f218f34ce08ecbee90fbc869c849bb017f
shakeel7521951 <shakeel7521951@gmail.com>
2026-03-17 11:44:30 +0500

    feat: update API response format in getHello method and change default port to 3000

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response) {
+    res.format({
+      'text/html': () => res.send('<h1>Hello World! API is running</h1>'),
+      'application/json': () => res.json({ message: this.appService.getHello() }),
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,7 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  const port = configService.get<number>('PORT') || 5000;
+  const port = configService.get<number>('PORT') || 3000;
 
   await app.listen(port, '0.0.0.0');
 
